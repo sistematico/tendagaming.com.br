1d7cdbdbb3828cc6e662994b34b3f03d3d58a5e8
Lucas Saliés Brum <sistematico@gmail.com>
2024-09-30 21:01:48 -0400

    Update

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const site = {
   title: import.meta.env.PUBLIC_APP_NAME || 'Tenda',
-  description: import.meta.env.PUBLIC_APP_NAME || 'Multigaming Community',
+  description: import.meta.env.PUBLIC_APP_DESC || 'Multigaming Community',
   favicon: '/images/favicon.svg', // required
   ogp: '/images/ogp.png', // required
   url: import.meta.env.PUBLIC_APP_URL || 'https://tendagaming.com.br', // required

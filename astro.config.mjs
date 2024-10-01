62ca166d1fcc057aca614f9f2d9a30bfde9fd7fe
Lucas Saliés Brum <sistematico@gmail.com>
2024-09-30 20:18:39 -0400

    SASS deprecation warning

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,13 @@ export default defineConfig({
   site: 'https://tendagaming.com.br',
   // outDir: 'public',
   // publicDir: 'static',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler' // or "modern"
+        }
+      }
+    }
+  }
 });
